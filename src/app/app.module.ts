e5f5b25621a5a1f63da6712705c8ea45b82cda88
Mun Chun <neroarth@gmail.com>
2023-02-27 20:35:11 +0800

    using bootstrapmade template

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,24 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
+import { HeroComponent } from './hero/hero.component';
+import { AboutUsComponent } from './about-us/about-us.component';
+import { ProductListComponent } from './product-list/product-list.component';
+import { EventComponent } from './event/event.component';
+import { ContactUsComponent } from './contact-us/contact-us.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    HeroComponent,
+    AboutUsComponent,
+    ProductListComponent,
+    EventComponent,
+    ContactUsComponent
   ],
   imports: [
     BrowserModule,
